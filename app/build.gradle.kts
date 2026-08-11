93140a0bfb7ddf54def9a034ed07c290a5443a29
Takotsubo <94740441+TakotsuboChen@users.noreply.github.com>
2026-08-12 00:19:06 +0800

    chore(release): bump to 1.0.0 Beta 3 (versionCode 100230)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "tools.alamobile.mod"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100220
-        versionName = "1.0.0 Beta 2"
+        versionCode = 100230
+        versionName = "1.0.0 Beta 3"
     }
 
     signingConfigs {
